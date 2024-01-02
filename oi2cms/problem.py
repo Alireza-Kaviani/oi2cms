225d773f543183f9fac5c0e820b1180e05c0f45e
Alireza Kaviani <alireza.kaviani.2005@gmail.com>
2024-01-02 09:29:47 +0330

    Add USACO exporter

--- oi2cms/problem.py
+++ oi2cms/problem.py
@@ -21,6 +21,28 @@ def export_coci_problem(path:str, checker:str = None):
         problem.checker = checker
     export_cms(problem) #TODO: Add path
 
+@app.command(name="usaco-problem")
+def export_coci_problem(path:str, checker:str = None):
+    name = path.split("/")[-1]
+    all_testcases = []
+    n_subtasks = int(input("Enter number of subtasks: "))
+    for i in range(n_subtasks + 1):
+        pattern = input(f"Enter testcases in subtask {i}. (separate ranges by , if needed e.g 1-5,7-10)\n")
+        testcases = []
+        for test_range in pattern.split(","):
+            trange = list(map(int, test_range.split("-")))
+            if(len(trange) == 1):
+                testcases.append(str(trange[0]))
+            else:
+                for j in range(trange[0], trange[1] + 1):
+                    testcases.append(str(j))
+        in_pattern = f"{path}/(?P<test>{'|'.join(testcases)})\\.in"
+        out_pattern = f"{path}/(?P<test>{'|'.join(testcases)})\\.out"
+        all_testcases += find_specific_subtask(path, str(i), in_pattern, out_pattern)
+    problem = Problem(name, name, all_testcases)
+    if(checker != None):
+        problem.checker = checker
+    export_cms(problem)
 
 @app.command()
 def hello():
